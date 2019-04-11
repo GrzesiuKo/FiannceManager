131dfe578045ccf9160a8a69c07db0935d739515
GrzesiuKo <gak@autograf.pl>
2019-04-11 22:05:43 +0200

    Divided expense adding into GET and POST

--- src/main/java/com/pw/controller/AddExpenseController.java
+++ src/main/java/com/pw/controller/AddExpenseController.java
@@ -4,12 +4,19 @@ import com.pw.model.Expense;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AddExpenseController {
-    @RequestMapping(value = "/addExpense")
-    public String addExpense(@ModelAttribute("expense") Expense expense){
+    @RequestMapping(value = "/addExpense", method = RequestMethod.GET)
+    public String showPage(@ModelAttribute("expense") Expense expense){
         System.out.println("Expense: "+expense.getCost());
         return "addExpense";
+    }
+
+    @RequestMapping(value = "/addExpense", method = RequestMethod.POST)
+    public String addGoal(@ModelAttribute("expense") Expense expense){
+        System.out.println("Added an expense of: "+expense.getCost());
+        return "redirect:homesweethome.html";
     }
 }
