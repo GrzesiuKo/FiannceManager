22c8599a076ea4dc16505939d070364efb1f2777
GrzesiuKo <gak@autograf.pl>
2019-04-11 19:02:43 +0200

    Added Expense and Category models

---
+++ src/main/java/com/pw/model/Category.java
@@ -0,0 +1,13 @@
+package com.pw.model;
+
+public class Category {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/pw/model/Expense.java
@@ -0,0 +1,22 @@
+package com.pw.model;
+
+public class Expense {
+    private Category category;
+    private double cost;
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
