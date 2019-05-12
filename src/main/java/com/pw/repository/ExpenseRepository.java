17c03582f2357d9db9a7baf2388557fe90fbd8ac
GrzesiuKo <gak@autograf.pl>
2019-05-12 18:24:37 +0200

    Added insert statement

--- src/main/java/com/pw/repository/ExpenseRepository.java
+++ src/main/java/com/pw/repository/ExpenseRepository.java
@@ -1,11 +1,34 @@
 package com.pw.repository;
 
 import com.pw.model.Expense;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.stereotype.Repository;
+
+import java.sql.*;
 
 @Repository("expenseRepository")
 public class ExpenseRepository {
-    public Expense addExpense(Expense expense) {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public Expense addExpense(final Expense expense) {
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement = connection.prepareStatement("insert INTO expenses(date, category, name, money) values (?,?,?,?,?)", new String[]{"id"});
+
+                preparedStatement.setTimestamp(1, expense.getDate());
+
+                preparedStatement.setString(2,expense.getCategory());
+                preparedStatement.setString(3, expense.getName());
+                preparedStatement.setDouble(4, expense.getMoney());
+
+                return preparedStatement;
+            }
+        });
 
         return null;
     }
