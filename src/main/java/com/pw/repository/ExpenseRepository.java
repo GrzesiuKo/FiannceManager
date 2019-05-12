9a18435741029f6faa963ff0ea53256253b3efb4
GrzesiuKo <gak@autograf.pl>
2019-05-12 16:32:19 +0200

    Added gist of addExpense in Service and Repository

--- src/main/java/com/pw/repository/ExpenseRepository.java
+++ src/main/java/com/pw/repository/ExpenseRepository.java
@@ -1,4 +1,12 @@
 package com.pw.repository;
 
+import com.pw.model.Expense;
+import org.springframework.stereotype.Repository;
+
+@Repository("expenseRepository")
 public class ExpenseRepository {
+    public Expense addExpense(Expense expense) {
+
+        return null;
+    }
 }
--- src/main/java/com/pw/service/ModifyExpenseService.java
+++ src/main/java/com/pw/service/ModifyExpenseService.java
@@ -1,12 +1,19 @@
 package com.pw.service;
 
 import com.pw.model.Expense;
+import com.pw.repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("modifyExpenseService")
 public class ModifyExpenseService {
+
+    @Autowired
+    ExpenseRepository expenseRepository;
+
     public Expense addExpense(Expense expense) {
 
-        return null;
+        expenseRepository.addExpense(expense);
+
     }
 }
